62c4ef58d4eb2b252e93111f8ee327036d4103ba
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2022-11-28 17:19:31 +1100

    register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,88 @@
-
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 
 function Register() {
-  return (
-    <div>
-        <h1>Register</h1>
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
 
-    </div>
-  )
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const { name, email, password, password2 } = formData;
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Inscription
+        </h1>
+        <p>Merci de créer un compte</p>
+      </section>
+
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              className="form-control"
+              name="name"
+              type="text"
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Entrer votre nom"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              name="email"
+              type="mail"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Entrer votre email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              name="password"
+              type="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Entrer votre mot de passe"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              className="form-control"
+              name="password2"
+              type="password"
+              id="password2"
+              value={password2}
+              onChange={onChange}
+              placeholder="Confirmer votre mot de passe"
+            />
+          </div>
+          <div className="form-group">
+            <button className=" btn btn-block">S'inscrire</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Register
+export default Register;
