f258c9d5e1940ab39a64156fbe0a5e5c689ca923
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2022-12-12 00:54:08 +1100

    profil-page

--- src/pages/private/PrivateHome.jsx
+++ src/pages/private/PrivateHome.jsx
@@ -1,43 +1,47 @@
-
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { getProfil, reset } from '../../features/user/userSlice'
-import Spinner from '../../components/shared/spinner/Spinner'
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getProfil, reset } from "../../features/user/userSlice";
+import Spinner from "../../components/shared/spinner/Spinner";
+import { Link } from "react-router-dom";
 
 function PrivateHome() {
-
-
   const { profil, isLoading, isError, isSuccess } = useSelector(
-    (state) => state.profil,
-  )
-  const dispatch = useDispatch()
+    (state) => state.profil
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
     return () => {
       if (isSuccess) {
-        dispatch(reset())
+        dispatch(reset());
       }
-    }
-  }, [dispatch, isSuccess, isError])
+    };
+  }, [dispatch, isSuccess, isError]);
 
   useEffect(() => {
-    dispatch(getProfil())
-  }, [])
+    dispatch(getProfil());
+  }, []);
 
-
-console.log(profil.data);
+  console.log(profil.data);
 
   if (!profil.data || isLoading) {
-    return <Spinner />
+    return <Spinner />;
   } else {
-  return (
-    <div className="page-content">
- <section className="heading">
-  <h1>bienvenue</h1>
- </section>
-    </div>
-  )
-}
+    return (
+      <div className="page-content">
+        <section className="heading">
+          <h1>Bienvenue {profil.data.name} Nous sommes content de vous revoir</h1>
+        </section>
+        <div className="collectData">
+          <p className="nb-collect">3 collectes effectuer</p>
+          <p><span>45 Kilos</span> de plastiques recyclé grâce vous !</p>
+        </div>
+        <Link to={'/new-request'} className='btn  btn-block'>
+          Faire une demande de collecte
+      </Link>
+      </div>
+    );
+  }
 }
 
-export default PrivateHome
+export default PrivateHome;
